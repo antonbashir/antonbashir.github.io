cf302543fa205bae6970e350ca01287c221d9918
anton.bashirov <anton.sh.local@gmail.com>
2023-12-18 13:49:57 +0200

    fixes

--- src/components/Animation.tsx
+++ src/components/Animation.tsx
@@ -4,15 +4,20 @@ import React, { useEffect, useRef } from "react";
 
 function MobileNav() {
   const app = useRef<HTMLDivElement>(null);
-
-
   useEffect(() => {
-    gsap.to(app.current, { rotate: 360, duration: 5,  })
+    gsap.to(app.current, {
+      rotation: 360,
+      x: '100vw',
+      xPercent: -100,
+      duration: 2,
+      repeat: -1,
+      ease: "power1.out",
+      yoyo: true,
+    });
   }, [])
-
   return (
     <>
-      <div className="rectangle" ref={app}>Hello World</div>
+      <div className="rectangle" ref={app}></div> 
     </>
   )
 }
