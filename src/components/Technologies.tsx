8a9080d40a9be72f540498c250c5b329633b4ecd
anton.bashirov <anton.sh.local@gmail.com>
2023-12-31 01:04:18 +0200

    techno

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -4,10 +4,16 @@ import 'swiper/css';
 import 'swiper/css/effect-fade';
 import '../styles/swiper.css';
 
+export enum TechnologyMode {
+  languages,
+  applications
+}
+
 type Properties = {
   spaceBetween: number
   slidesPerView: number
   technologies: Array<string>
+  mode: TechnologyMode
 }
 
 export const Technologies = (properties: Properties) => {
@@ -16,20 +22,26 @@ export const Technologies = (properties: Properties) => {
     freeMode
     className='swiper-container'
     slidesPerView={properties.slidesPerView}
-    speed={5000}
+    speed={3000}
     autoplay={{
-      delay: 1,
+      delay: 0,
       disableOnInteraction: false,
     }}
     spaceBetween={properties.spaceBetween}
     modules={[Grid, Autoplay, Pagination]}>
     {properties.technologies.map((technology) => <SwiperSlide>
-      <div className="rounded-3xl p-0.5 bg-gradient-to-r from-light-blue-500 via-blue-500 to-indigo-500">
-        <div className='flex place-content-center p-8 rounded-3xl bg-base-200'>
-          <p className="text-lg font-bold bg-gradient-to-r from-light-blue-500 via-blue-500 to-indigo-500 inline-block text-transparent bg-clip-text">{technology}</p>
+      {properties.mode == TechnologyMode.languages
+        ? <div className="rounded-3xl p-0.5 bg-gradient-to-r from-light-blue-500 via-blue-500 to-indigo-500">
+          <div className='flex place-content-center p-8 rounded-3xl bg-base-200'>
+            <p className="text-lg font-bold bg-gradient-to-r from-light-blue-500 via-blue-500 to-indigo-500 inline-block text-transparent bg-clip-text">{technology}</p>
+          </div>
         </div>
-
-      </div>
+        : <div className="rounded-3xl p-0.5 bg-gradient-to-r from-orange-500 via-yellow-500 to-red-500">
+          <div className='flex place-content-center p-8 rounded-3xl bg-base-200'>
+            <p className="text-lg font-bold bg-gradient-to-r from-orange-500 via-yellow-500 to-red-500 inline-block text-transparent bg-clip-text">{technology}</p>
+          </div>
+        </div>
+      }
     </SwiperSlide>)}
   </Swiper>
 }
