f80d74205512d8721aa16576f0c662c45c4e05ba
anton.bashirov <anton.sh.local@gmail.com>
2023-12-30 20:51:48 +0200

    site

--- src/components/Cat.tsx
+++ src/components/Cat.tsx
@@ -14,6 +14,6 @@ export const CatAnimation = (props: Properties) => {
 
   return (
     <RiveComponent
-      style={{ height: "35rem", width: "35rem" }}
+      style={{ height: "150px", width: "150px" }}
     />)
 }
