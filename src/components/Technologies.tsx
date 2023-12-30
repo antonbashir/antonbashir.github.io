29d79843418aff787d25cbb6a5168aa71812c64c
anton.bashirov <anton.sh.local@gmail.com>
2023-12-31 01:41:52 +0200

    technologies

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -10,38 +10,36 @@ export enum TechnologyMode {
 }
 
 type Properties = {
+  key?: string
   spaceBetween: number
   slidesPerView: number
   technologies: Array<string>
   mode: TechnologyMode
 }
 
-export const Technologies = (properties: Properties) => {
-  return <Swiper
-    loop
-    freeMode
-    className='swiper-container'
-    slidesPerView={properties.slidesPerView}
-    speed={3000}
-    autoplay={{
-      delay: 0,
-      disableOnInteraction: false,
-    }}
-    spaceBetween={properties.spaceBetween}
-    modules={[Grid, Autoplay, Pagination]}>
-    {properties.technologies.map((technology) => <SwiperSlide>
-      {properties.mode == TechnologyMode.languages
-        ? <div className="rounded-3xl p-0.5 bg-gradient-to-r from-light-blue-500 via-blue-500 to-indigo-500">
-          <div className='flex place-content-center p-8 rounded-3xl bg-base-200'>
-            <p className="text-lg font-bold bg-gradient-to-r from-light-blue-500 via-blue-500 to-indigo-500 inline-block text-transparent bg-clip-text">{technology}</p>
-          </div>
+export const Technologies = (properties: Properties) => <Swiper
+  loop
+  freeMode
+  className='swiper-container'
+  slidesPerView={properties.slidesPerView}
+  speed={3000}
+  autoplay={{
+    delay: 0,
+    disableOnInteraction: false,
+  }}
+  spaceBetween={properties.spaceBetween}
+  modules={[Grid, Autoplay, Pagination]}>
+  {properties.technologies.map((technology) => <SwiperSlide>
+    {properties.mode == TechnologyMode.languages
+      ? <div className="rounded-3xl p-0.5 bg-gradient-to-r from-light-blue-500 via-blue-500 to-indigo-500">
+        <div className='flex place-content-center p-8 rounded-3xl bg-base-200'>
+          <p className="text-lg font-bold bg-gradient-to-r from-light-blue-500 via-blue-500 to-indigo-500 inline-block text-transparent bg-clip-text">{technology}</p>
         </div>
-        : <div className="rounded-3xl p-0.5 bg-gradient-to-r from-orange-500 via-yellow-500 to-red-500">
-          <div className='flex place-content-center p-8 rounded-3xl bg-base-200'>
-            <p className="text-lg font-bold bg-gradient-to-r from-orange-500 via-yellow-500 to-red-500 inline-block text-transparent bg-clip-text">{technology}</p>
-          </div>
+      </div>
+      : <div className="rounded-3xl p-0.5 bg-gradient-to-r from-orange-500 via-yellow-500 to-red-500">
+        <div className='flex place-content-center p-8 rounded-3xl bg-base-200'>
+          <p className="text-lg font-bold bg-gradient-to-r from-orange-500 via-yellow-500 to-red-500 inline-block text-transparent bg-clip-text">{technology}</p>
         </div>
-      }
-    </SwiperSlide>)}
-  </Swiper>
-}
+      </div>}
+  </SwiperSlide>)}
+</Swiper>
