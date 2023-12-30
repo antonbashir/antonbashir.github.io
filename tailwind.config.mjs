67adff8cbd0cd9c69c542c0c25c223c2cfbf93cf
anton.bashirov <anton.sh.local@gmail.com>
2023-12-30 05:35:22 +0200

    site

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,39 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {
-      screens: {
-        sm: '480px',
-        md: '768px',
-        lg: '976px',
-        xl: '1440px',
-      },
-      colors: {
-        'blue': '#1fb6ff',
-        'purple': '#7e5bef',
-        'pink': '#ff49db',
-        'orange': '#ff7849',
-        'green': '#13ce66',
-        'yellow': '#ffc82c',
-        'gray-dark': '#273444',
-        'gray': '#8492a6',
-        'gray-light': '#d3dce6',
-      },
-      fontFamily: {
-        sans: ['Inter'],
-        serif: ['Inter'],
-      },
-      extend: {
-        spacing: {
-          '128': '32rem',
-          '144': '36rem',
-        },
-        borderRadius: {
-          '4xl': '2rem',
-        }
-      }
-    },
+  plugins: [
+    require("daisyui")
+  ],
+
+  daisyui: {
+    themes: ["dark"],
+    base: true,
+    styled: true,
+    utils: true,
+    prefix: "",
+    logs: true,
+    themeRoot: ":root",
   },
-  plugins: [],
 }
