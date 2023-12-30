c82ed68f42cba205c3b2de3e3b4c15f266abd339
anton.bashirov <anton.sh.local@gmail.com>
2023-12-30 18:17:45 +0200

    site

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,13 +9,46 @@ export default {
 
   daisyui: {
     themes: [{
-      dark: {
-        ...require("daisyui/src/theming/themes")["dark"],
-        "primary": "white",
-        "base-100": "#000000",
-        "base-200": "#e6e6e6",
-        "base-300": "000000",
-      }
+      'black': {
+        'primary': '#ffffff',
+        'primary-focus': '#ffffff',
+        'primary-content': '#000000',
+
+        'secondary': '#ffffff',
+        'secondary-focus': '#ffffff',
+        'secondary-content': '#000000',
+
+        'accent': '#ffffff',
+        'accent-focus': '#ffffff',
+        'accent-content': '#000000',
+
+        'neutral': '#333333',
+        'neutral-focus': '#4d4d4d',
+        'neutral-content': '#ffffff',
+
+        'base-100': '#000000',
+        'base-200': '#333333',
+        'base-300': '#4d4d4d',
+        'base-content': '#ffffff',
+
+        'info': '#0000ff',
+        'success': '#008000',
+        'warning': '#ffff00',
+        'error': '#ff0000',
+
+        '--animation-btn': '0',
+        '--animation-input': '0',
+
+        '--btn-text-case': 'lowercase',
+        '--navbar-padding': '.5rem',
+        '--border-btn': '1px',
+        ".input:focus": {
+          "border-color": "none",
+          "outline-color": "none",
+          "outline-style": "none",
+          "outline-width": "none",
+        }
+      },
     }],
     base: true,
     styled: true,
