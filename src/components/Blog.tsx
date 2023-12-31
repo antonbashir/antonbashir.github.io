aea528bf0c665d19f1be332d0bc4eff24900c9fc
anton.bashirov <anton.sh.local@gmail.com>
2023-12-31 16:48:56 +0200

    fixes

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -11,7 +11,7 @@ export const Article = (props: Properties) => <a
     className="hero-content card flex flex-row"
   >
     <svg
-      className="h-32 w-32 fill-current"
+      className="h-16 w-16 fill-current"
       xmlns="http://www.w3.org/2000/svg"
       x="0px"
       y="0px"
@@ -23,7 +23,7 @@ export const Article = (props: Properties) => <a
         d="M 7 2 L 7 48 L 43 48 L 43 14.59375 L 42.71875 14.28125 L 30.71875 2.28125 L 30.40625 2 Z M 9 4 L 29 4 L 29 16 L 41 16 L 41 46 L 9 46 Z M 31 5.4375 L 39.5625 14 L 31 14 Z M 15 22 L 15 24 L 35 24 L 35 22 Z M 15 28 L 15 30 L 31 30 L 31 28 Z M 15 34 L 15 36 L 35 36 L 35 34 Z"
       ></path>
     </svg>
-    <div className="card-body place-items-start">
+    <div className="card-body place-items-start p-2">
       <div className="card-title">{props.title}</div>
       <p>{props.description}</p>
     </div>
