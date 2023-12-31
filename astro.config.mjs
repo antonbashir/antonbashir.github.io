a381659a08c4d4c347217b61049a077b22bcc502
anton.bashirov <anton.sh.local@gmail.com>
2023-12-31 06:53:12 +0200

    robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import robotsTxt from 'astro-robots-txt';
 
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  site: 'https://antonbashir.github.io',
+  integrations: [tailwind(), react(), robotsTxt()],
 });
