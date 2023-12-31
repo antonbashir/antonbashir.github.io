9cdf139727da7e3e5d1e4128b08b789551edfc9f
anton.bashirov <anton.sh.local@gmail.com>
2023-12-31 20:39:50 +0200

    contact

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import react from "@astrojs/react";
 import robotsTxt from 'astro-robots-txt';
 
 export default defineConfig({
+  output: 'hybrid',
   site: 'https://antonbashir.github.io',
   integrations: [tailwind(), react(), robotsTxt()],
 });
