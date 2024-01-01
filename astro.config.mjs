a7ee00c0634958ee0f469ef664e2487dd3930de7
anton.bashirov <anton.sh.local@gmail.com>
2024-01-01 15:54:36 +0200

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import robotsTxt from 'astro-robots-txt';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://antonbashir.github.io',
-  integrations: [tailwind(), react(), robotsTxt()],
+  integrations: [tailwind(), react(), robotsTxt(), sitemap()],
 });
