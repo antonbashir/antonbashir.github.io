12eeef2e769397905af411066de1b2eafddf657e
anton.bashirov <anton.sh.local@gmail.com>
2024-01-06 23:10:01 +0200

    fixes

--- src/components/ExperiencePath.tsx
+++ src/components/ExperiencePath.tsx
@@ -2,6 +2,5 @@ import { ExperienceDesktop } from "./ExperienceDesktop";
 import { ExperienceMobile } from "./ExperienceMobile";
 
 export const ExperiencePath = () => {
-  console.log(window.innerWidth);
   return window.innerWidth > 1320 ? <ExperienceDesktop /> : <ExperienceMobile />;
 }
